8a205214f02d13c477431d50766399f4183ae48c
Amy Smith <asmith@liquibase.com>
2026-08-27 09:57:45 -0500

    Adding parameters

--- cicd/Jenkinsfile.groovy
+++ cicd/Jenkinsfile.groovy
@@ -21,7 +21,20 @@ pipeline {
     PATH="$PATH:/opt/liquibase/liquibase:$DB2_HOME/bin:$UCD_COMMAND_HOME"
     BASE_VERSION="50"
     EMAIL="asmith@liquibase.com"
- 
+
+    LIQUIBASE_COMMAND_CHANGELOG_FILE="db.changelog-main.yaml"
+    LIQUIBASE_COMMAND_URL="jdbc:db2://db2-luw.liquibase.net:50000/GDITREF5"
+    LIQUIBASE_LIQUIBASE_SCHEMA_NAME="SECURE_TRACKING"
+    LIQUIBASE_COMMAND_DEFAULT_SCHEMA_NAME="NYHBEODB_929"
+
+    LIQUIBASE_LOG_FORMAT="JSON_PRETTY"
+    LIQUIBASE_LOG_FILE="logs/log.json"
+    LIQUIBASE_LOG_LEVEL: "INFO"
+
+    LIQUIBASE_DBCLHISTORY_ENABLED="true"
+    LIQUIBASE_REPORTS_PATH="reports"
+    LIQUIBASE_COMMAND_CHECKS_RUN_AUTO_UPDATE="false"
+
   }
 	
   stages {
@@ -90,7 +103,7 @@ pipeline {
       steps {
 
 		 		withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'DB2Login',
-									usernameVariable: 'DB2_SECURE_USER', passwordVariable: 'DB2_SECURE_PASS']]) {
+									usernameVariable: 'LIQUIBASE_COMMAND_USERNAME', passwordVariable: 'LIQUIBASE_COMMAND_PASSWORD']]) {
 
 					sh '''
 					  { set +x; } 2>/dev/null				
@@ -156,13 +169,15 @@ pipeline {
     } // always
 
     success {
+        echo "Build succeeded for WI ${env.GIT_WORK_ITEM}"
         // Email Success Log To Developer
-        emailext attachmentsPattern: '**/Reports/**/*.html', attachLog: false, body: '${BUILD_STATUS}: ${JOB_NAME} for ${work_item} build ${BUILD_NUMBER}', subject: 'Build ${BUILD_STATUS}: Job ${JOB_NAME} Build ${BUILD_NUMBER}', to: '${EMAIL}'
+        // emailext attachmentsPattern: '**/Reports/**/*.html', attachLog: false, body: '${BUILD_STATUS}: ${JOB_NAME} for ${work_item} build ${BUILD_NUMBER}', subject: 'Build ${BUILD_STATUS}: Job ${JOB_NAME} Build ${BUILD_NUMBER}', to: '${EMAIL}'
     } // success
 
     unsuccessful {
+        echo "Build did not succeed for WI ${env.GIT_WORK_ITEM}"
         // Email Failure Logs To Developer
-        emailext attachmentsPattern: '**/Reports/**/*.html', attachLog: true, body: '${BUILD_STATUS}: ${JOB_NAME} for ${work_item} build ${BUILD_NUMBER} Failure: Use the attached console log to see the specific error (Tip: search "error" in the text log)', subject: 'Build ${BUILD_STATUS}: Job ${JOB_NAME} Build ${BUILD_NUMBER}', to: '${EMAIL}'
+        // emailext attachmentsPattern: '**/Reports/**/*.html', attachLog: true, body: '${BUILD_STATUS}: ${JOB_NAME} for ${work_item} build ${BUILD_NUMBER} Failure: Use the attached console log to see the specific error (Tip: search "error" in the text log)', subject: 'Build ${BUILD_STATUS}: Job ${JOB_NAME} Build ${BUILD_NUMBER}', to: '${EMAIL}'
     } // unsuccessful
 	  
     cleanup { 
