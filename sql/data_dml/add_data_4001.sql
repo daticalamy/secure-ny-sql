39536e9bacd323096f89710a0c7639d57f62fc1a
Amy Smith <asmith@liquibase.com>
2026-08-27 14:57:14 -0500

    WI 393841 adding test data

--- sql/data_dml/add_data_4001.sql
+++ sql/data_dml/add_data_4001.sql
@@ -11,3 +11,6 @@ INSERT INTO new_emp14 (id, name, jobrole) VALUES (4004,'Clairo','Singer');
 
 --changeset asmith:table_001_insert_4005 labels:"WI 393840"
 INSERT INTO new_emp14 (id, name, jobrole) VALUES (4005,'Clairo','Singer'); 
+
+--changeset asmith:table_001_insert_4006 labels:"WI 393841"
+INSERT INTO new_emp14 (id, name, jobrole) VALUES (4006,'Clairo','Singer'); 
